b23687649a3c05a0141e24641d6bdbf5795ca1ae
Sirebellum <joshua.herrera227@gmail.com>
2018-09-14 04:01:18 -0500

    Fixed dimensions issue

--- mnist.py
+++ mnist.py
@@ -7,7 +7,7 @@ with open('mnist_test.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         test_labels.append(row[0])
-        image = [ row[0+x:27+x] for x in range(0, 28*28, 28) ]
+        image = [ row[0+x:28+x] for x in range(0, 28*28, 28) ]
         test_images.append(image)
         
 train_labels = list()
@@ -16,21 +16,23 @@ with open('mnist_train.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         train_labels.append(row[0])
-        image = [ row[0+x:27+x] for x in range(0, 28*28, 28) ]
+        image = [ row[0+x:28+x] for x in range(0, 28*28, 28) ]
         train_images.append(image)
         
-# COnvert to numpy arrays
-test_labels = np.asarray(test_labels, dtype=np.uint8)
-test_images = np.asarray(test_images, dtype=np.uint8)
-train_labels = np.asarray(train_labels, dtype=np.uint8)
-train_images = np.asarray(train_images, dtype=np.uint8)
+# Convert to numpy arrays
+test_labels = np.asarray(test_labels, dtype=np.dtype('int32'))
+test_images = np.asarray(test_images, dtype=np.dtype('float16'))
+train_labels = np.asarray(train_labels, dtype=np.dtype('int32'))
+train_images = np.asarray(train_images, dtype=np.dtype('float16'))
+# Convert to float
+test_images = np.divide(test_images, 255)
+train_images = np.divide(train_images, 255)
 
 #import ipdb; ipdb.set_trace()
 
 if __name__ == "__main__":
     from PIL import Image
-    for instance in test_images:
-        img = Image.fromarray(instance, 'L')
+    for data in test_images:
+        img = Image.fromarray(data, 'L')
         img.show()
         input()
-        img.close()
